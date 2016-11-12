97e879c145d531216cbe4a84206d0676c817fc3e
Forrest Zhao <fozhao92@gmail.com>
2016-11-12 14:08:34 -0500

    init firebase, add crumb and location data models, add two vc managed by tab control

---
+++ BreadCrumbs/Crumb.swift
@@ -0,0 +1,38 @@
+//
+//  Crumb.swift
+//  BreadCrumbs
+//
+//  Created by Forrest Zhao on 11/12/16.
+//  Copyright © 2016 ForrestApps. All rights reserved.
+//
+
+import UIKit
+
+struct Crumb {
+    
+    var name: String
+    var rating: Int?
+    var locations: [Location] = []
+    
+    init(name: String) {
+        self.name = name
+    }
+
+}
+
+struct Location {
+    
+    var name: String
+    var latitude: Double
+    var longitude: Double
+    var placeInLine: Int
+    var image: UIImage?
+    var description: String?
+    
+    init(name: String, latitude: Double, longitude: Double, placeInLine: Int) {
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
+        self.placeInLine = placeInLine
+    }
+}
---
+++ BreadCrumbs/Enums.swift
@@ -0,0 +1,12 @@
+//
+//  Rating.swift
+//  BreadCrumbs
+//
+//  Created by Forrest Zhao on 11/12/16.
+//  Copyright © 2016 ForrestApps. All rights reserved.
+//
+
+enum Rating: Int {
+    case one = 1, two = 2, three = 3, four = 4, five = 5
+}
+
